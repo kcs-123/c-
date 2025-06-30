ca1d313611172de855f00858cdefba3109ef7b6f
kcs-123 <kimchansik6@gmail.com>
2025-06-30 19:51:15 +0900

    Add files via upload

---
+++ 0630 6.cpp
@@ -0,0 +1,46 @@
+//#include<stdio.h>
+//int main()
+//{
+//	char command;
+//	printf("<,a 왼쪽 이동\n");
+//	printf(">,d 오른쪽이동\n");
+//	scanf("%c", &command);
+//	switch (command)
+//	{
+//	case'<':
+//	case'a':
+//		printf("왼쪽으로 이동합니다.\n");
+//		break;
+//	case'>': case'd': 
+//		printf("오른쪽으로 이동합니다.\n");
+//		break;
+//	}
+//	return 0;
+//}
+//#include<stdio.h>
+//int main()
+//{
+//	int input;
+//	const int option = 3;
+//	printf("메뉴를 선택하세요");
+//	printf("1.새 게임 2.이어하기 3.옵션\n");
+//	scanf("%d", &input);
+//	switch(input)
+//	{
+//	case 1: printf("새 게임 시작.\n");
+//		break;
+//	case 2: printf("세이브 데이터 로드.\n");
+//		break;
+//	case option: printf("옵션 세팅.\n");
+//		break;
+//	default:
+//		printf("지정된 기능이 없습니다.\n");
+//	}
+//	return 0;
+//}    //const를 통해 상수를 지정할 수 있다.
+//문제5
+#include<stdio.h>
+int main()
+{
+	printf("")
+}
